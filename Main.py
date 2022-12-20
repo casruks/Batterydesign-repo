e3b4aa68fb9dc32b6fb9948f8ea3a1a9aff0efb4
casruks <81488457+casruks@users.noreply.github.com>
2022-12-20 10:05:47 +0100

    Update

--- Main.py
+++ Main.py
@@ -1,107 +1,137 @@
 import os
+import re
 import digikey
-from digikey.v3.productinformation import KeywordSearchRequest, ParametricFilter
-from py3dbp import Packer, Bin, Item     #used for optimal location  
-import numpy as np
 import math as m
-import requests
-import json
+from itertools import chain
+from digikey.v3.productinformation import KeywordSearchRequest
+from py3dbp import Packer, Bin, Item     #used for optimal location  
 
+### Battery Dimensions in [mm] ###
+H_box = 42 
+W_box = 34
+L_box = 33.8
+Ah = 600e-3
+V = 3.7
+s = 3600
+E = Ah*V*s
 
 os.environ['DIGIKEY_CLIENT_ID'] = 'rGRVvZwhTTBu8LZrov7v6CbEoAlbuaRL'
 os.environ['DIGIKEY_CLIENT_SECRET'] = 'CEIUIRl5vQ2m4pV6'
 os.environ['DIGIKEY_CLIENT_SANDBOX'] = 'False'
 os.environ['DIGIKEY_STORAGE_PATH'] = 'C:\\Users\\casru\\Documents\\GitHub\\Batterydesign-repo\\tmp'
 
-# Query product number
-dkpn = '2085-PBLC-3R8/220MA2-ND'
-part = digikey.product_details(dkpn, x_digikey_locale_site='NL', x_digikey_locale_currency='EUR')
+def findIndex(i):
+    for n in range(len(result.products[i].parameters)):
+        if result.products[i].parameters[n].parameter_id == 2049: #capacitance
+            i_C = n
+        else:
+            i_C = 'None found'
+        if result.products[i].parameters[n].parameter_id == 46: #size/dim
+            i_D = n
+        else:
+            i_D = 'None found'
+        if result.products[i].parameters[n].parameter_id == 1500: #height
+            i_H = n
+        else:
+            i_H = 'None found'
+        if result.products[i].parameters[n].parameter_id == 2079: #rated voltage
+            i_V = n
+        else: 
+            i_V = 'None found' #for i = 38 
+    return i_C, i_D, i_H, i_V
 
-# Capacitor data
-# part.parameters[1].value
-C = part.parameters[12].value
-D = part.parameters[3].value
-# L = part.parameters[10].value
-# V_n = part.parameters[13].value
-# Operating_temp = part.parameters[5].value
-print(C)
-# search_request = KeywordSearchRequest(keywords='Electric Double Layer Capacitors (EDLC), Supercapacitors', record_count=20, record_start_position=0, filters=ParametricFilter(parameter_id=46,value_id='21491'))
-# result = digikey.keyword_search(body=search_request, x_digikey_locale_site='NL', x_digikey_locale_currency='EUR')
-#result.products[nth product]
-# print(result.products[19].parameters)
-# g = result.products[19].parameters.index('{')
-# print(result.products[19].parameters[])
+#Definiton to extract string from data (applicable for D and L)
+def ExtractData_H(sampleStr): 
+    try :
+        # here ( and ) are our two markers  #'1.654" (42.00mm)' -> 42.00mm
+        # in which string can be found. 
+        marker1 = '\('
+        marker2 = '\)'
+        regexPattern = marker1 + '(.+?)' + marker2
+        str_found = re.search(regexPattern, sampleStr).group(1)
+    except AttributeError:
+        # Attribute error is expected if string 
+        # is not found between given markers
+        str_found = '0' #NOTHING FOUND CODE
+    return float(str_found.strip('mm')) 
 
-# print(C,D,L,V_n,Operating_temp)
-# resultlst = []
-# x = int(2104/8)  #2104
-# y = 8
-# for i in range(1): #54(x) times 39(y) results = 2106
-#     search_request = KeywordSearchRequest(keywords='Electric Double Layer Capacitors (EDLC), Supercapacitors', record_count=y, record_start_position=0+i*y, filters=ParametricFilter(parameter_id=2049,value_id='Capacitance'))
-#     result = digikey.keyword_search(body=search_request, x_digikey_locale_site='NL', x_digikey_locale_currency='EUR')
-#     print(str(i) + '/'+str(x)+' done..')
-#     resultlst.append(result)
-#     with open('Results\\result' + str(i) + '.txt', 'w') as f:
-#         f.write(str(result))
-          
+def ExtractData_D(sampleStr): 
+    if len(sampleStr.replace('x','')) < len(sampleStr):
+        # here ( and ) are our two markers  #'1.654" (42.00mm)' -> 42.00mm
+        # in which string can be found. 
+        marker1 = '\('
+        marker2 = ' x'
+        regexPattern = marker1 + '(.+?)' + marker2
+        L = re.search(regexPattern, sampleStr).group(1) 
+        marker3 = 'mm x '
+        marker4 = '\)'
+        regexPattern1 = marker3 + '(.+?)' + marker4
+        W = re.search(regexPattern1, sampleStr).group(1) 
+        outp = float(L.strip('mm')), float(W.strip('mm')) 
+    else:
+        # here ( and ) are our two markers  #'1.654" (42.00mm)' -> 42.00mm
+        # in which string can be found. 
+        marker1 = '\(' 
+        marker2 = '\)'
+        regexPattern = marker1 + '(.+?)' + marker2
+        str_found = re.search(regexPattern, sampleStr).group(1)
+        outp = float(str_found.strip('mm'))
+    return outp
 
-# with open('result.txt', 'w') as f:
-#     for i in range(len(resultlst)):
-#         if i == 0:
-#             f.write(str(resultlst[i]))
-#         else:
-#             f.write('\n'+'###'*24 + '\n' + str(resultlst[i]))
-            
-# for i in range(len(resultlst)):
-#     with open('Results\\result' + str(i) + '.txt', 'w') as f:
-#         f.write(str(resultlst[i]))
-      
+Clst, Hlst, Wlst, Llst, Vlst, dkpnlst, n_reqlst, C_reqlst, filtered_results = ([] for i in range(9))
 
-# V_1 = 4.2   #initial voltage
-# V_2 = 3     #final voltage
+x = int(m.ceil(2104/50))  #2104
+y = 50
+for i in range(43): #43(x) times (y) results = 2106
+    search_request = KeywordSearchRequest(keywords='Electric Double Layer Capacitors (EDLC), Supercapacitors', record_count=y, record_start_position=0+i*y)
+    result = digikey.keyword_search(body=search_request, x_digikey_locale_site='NL', x_digikey_locale_currency='EUR')
+    ist = i
+    for i in range(len(result.products)):
+        i_C, i_D, i_H, i_V = findIndex(i)
+        if type(i_C) and type(i_D) and type(i_H) and type(i_V) == int():
+            C = float(result.products[i].parameters[i_C].value.strip(' mF'))
+        
+            # D, two values (rectangle) or Diam (cylinder)
+            if type(ExtractData_D(result.products[i].parameters[i_D].value)) == tuple:
+                L, W = ExtractData_D(result.products[i].parameters[i_D].value)          
+            else:
+                L = W = ExtractData_D(result.products[i].parameters[i_D].value) #cyl
+        
+            H = ExtractData_H(result.products[i].parameters[i_H].value)
+            V_n = float(result.products[i].parameters[i_V].value.strip(' V'))
+            dkpn = result.products[i].digi_key_part_number
+            C_req = 2*(E)/V_n**2
+            n_req = C_req / C
+            packer = Packer()
+            packer.add_bin(Bin('Battery case', H_box, W_box, L_box, 999999))
+            for i in range(m.ceil(n_req)):
+                packer.add_item(Item('Capacitor' + str(i+1), H, W, L, 1+i))
+        
+            # packer.pack()
+            for i in packer.bins:
+                no_fitted = len(i.items)
+                no_unfitted = len(i.unfitted_items)
+                if no_fitted == m.ceil(n_req):
+                    Clst.append(C)
+                    Hlst.append(H)
+                    Wlst.append(W)
+                    Llst.append(L)
+                    Vlst.append(V_n)
+                    dkpnlst.append(dkpn)
+                    n_reqlst.append(n_req) 
+                    C_reqlst.append(C_req) 
+                    filtered_results.append(Clst)  
+                    filtered_results.append(Llst) 
+                    filtered_results.append(Vlst) 
+                    filtered_results.append(dkpnlst) 
+                    filtered_results.append(n_reqlst) 
+                    filtered_results.append(C_reqlst)
+        else:
+            i += 1 
+    print(str(ist+1) + '/' + str(x) + ' done..')
 
-# # Capacitor data PBLC-3R8/220MA2
-# C = 220 #F
-# D = 16
-# L = 25
-# V_n = 3.8  #nominal voltage
-
-# # Battery case
-# H = 40
-# W = 33.8
-# L = 35
-
-# def C_req(V_1, V_2):
-#     E = 600e-3 * V_n * 3600
-#     return 2*E/(V_1**2 - V_2**2)
-    
-# def pos_config(D, L, C, C_req):
-#     V_available = H*W*L  #mm3
-#     V = (np.pi*L*D**2)/4    #mm3
-#     n_cap = m.ceil(C_req(V_1,V_2)/C)
-#     if V_available<(V*n_cap):
-#         print('Capacitor provided too large')
-#     C_act = n_cap*C
-#     return C_act, n_cap
-
-# C_act, n_cap = pos_config(D, L, C, C_req)
-# print('C_act =', C_act, 'F')
-# print('n_cap =', n_cap)
-
-# my_bin = Bin(name, width, height, depth, max_weight)
-# my_item = Item(name, width, height, depth, weight)
-# packer = Packer()
-# packer.add_bin(Bin('Battery case', H, W, L, 999999))
-
-# for i in range(n_cap):
-#     packer.add_item(Item('Capacitor'+str(i+1), D, L, D, 1+i))
-    
-# packer.pack()
-# for b in packer.bins:
-#     print(b.string())
-#     print("\n FITTED ITEMS:")
-#     for item in b.items:
-#         print(item.string())
-#     print("\n UNFITTED ITEMS:")
-#     for item in b.unfitted_items:
-#         print(item.string())
+print('Total no of capacitors found =',result.products_count)
+print('No. of results after filter =', len(Clst))
+with open('Filtered_results.txt', 'w') as f:
+    for line in filtered_results:
+        f.write(f"{line}\n")
